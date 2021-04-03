6d4df6a9fdee29ca725c95899e0dd1c578d37402
AkaishaTulsyan <74300877+AkaishaTulsyan@users.noreply.github.com>
2021-04-03 12:51:29 +0530

    Add files via upload

---
+++ game.login.js
@@ -0,0 +1,7 @@
+function userlogin() {
+    player1_name = document.getElementById("player1Name").value;
+    player2_name = document.getElementById("player21Name").value;
+    localStorage.setItem("player1_name", player1_name);
+    localStorage.setItem("player2_name", player2_name);
+    window.location = "quiz.html";
+}
